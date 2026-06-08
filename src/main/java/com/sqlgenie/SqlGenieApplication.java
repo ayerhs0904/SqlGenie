b2ae7ef157b02889a9acb8412a9f462c9d6e5ce0
ayerhs0904 <shreya2212s@gmail.com>
2026-06-08 16:52:16 +0530

    feat: initialize Spring Boot project with Maven, add dependencies

---
+++ src/main/java/com/sqlgenie/SqlGenieApplication.java
@@ -0,0 +1,13 @@
+package com.sqlgenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlGenieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlGenieApplication.class, args);
+    }
+
+}
